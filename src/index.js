63bbdedf4f8ea667cba234864b4ab9be8b79375f
peanutyabing <sherry.liuyabing@gmail.com>
2023-06-15 17:06:04 +0800

    Add basic routing to questions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Question from "./Components/Question";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />}/>
+    <Route path="/question/:questionId" element={<Question />}/>
+  </Routes>
+  </BrowserRouter>
+);
