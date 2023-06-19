b2014a84b303078d4ce7e8d0636d4068a81d984f
hengmhs <hengmhs@gmail.com>
2023-06-19 23:46:27 +0800

    Added seedHints function to post hint data to firebase

--- src/App.js
+++ src/App.js
@@ -12,30 +12,6 @@ function App() {
   const navigate = useNavigate();
   const { setQuestionBank } = useQuestionBank();
 
-  // Create initial testing questions
-  // const seedData = () => {
-  //   const questionRef = ref(database, process.env.REACT_APP_DB_QUESTIONS_KEY);
-  //   const currentDate = new Date().toJSON();
-  //   push(questionRef, {
-  //     content: "hey this is a scam",
-  //     is_scam: true,
-  //     num_users_attempted: 0,
-  //     num_users_correct: 0,
-  //     date_created: currentDate,
-  //     bin_id: 1,
-  //   });
-  //   push(questionRef, {
-  //     content: "good news this isn't a scam",
-  //     is_scam: false,
-  //     num_users_attempted: 0,
-  //     num_users_correct: 0,
-  //     date_created: currentDate,
-  //     bin_id: 1,
-  //   });
-  //   // ... and more
-  //   // Docs: https://firebase.google.com/docs/database/web/read-and-write
-  // }
-
   useEffect(() => {
     const allQuestionsRef = ref(database, "questions");
     onValue(allQuestionsRef, (snapshot) => {
