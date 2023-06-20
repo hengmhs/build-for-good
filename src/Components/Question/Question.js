290d1da630bf1d664ccb1957af39b05b276312cd
markcwy-ra <rocket@markcwy.com>
2023-06-20 17:09:25 +0800

    Added sender styling, maintained mobile size on desktop

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,5 +1,16 @@
 import "./Question.css";
 
-export default function Question({ content }) {
-  return <div className="Question">{content}</div>;
+export default function Question({ content, sender }) {
+  return (
+    <div className="Pseudo-Phone">
+      <div className="CallerID">
+        <h4>{sender}</h4>
+      </div>
+      <div className="Question">{content}</div>
+      <div className="Input">
+        <div className="Input-TextBox"></div>
+        <div className="Input-Send"></div>
+      </div>
+    </div>
+  );
 }
